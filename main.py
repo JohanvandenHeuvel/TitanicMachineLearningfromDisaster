a6d83660f9e826170dd7bd78784c332d1a46feb5
johan van den heuvel <johan@debian.Kazan>
2016-12-06 20:21:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# import libraries
+
+import csv as csv
+import numpy as np
+
+# read file
+
+train = csv.reader(open("Files/train.csv", 'r'))
+trainHeader = train.__next__()
+data = []
+for row in train:
+    data.append(row)
+data = np.array(data)
+print(data)
